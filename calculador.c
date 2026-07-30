f20f0fa91673f17ebf83a6c2124b622c5f5633c5
Diogenes <dyogenes.duarte@gmail.com>
2026-07-30 00:09:11 -0300

    atualiza calculadora

---
+++ calculador.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+    char expressao[100];
+    int indice ;
+    int primeironumero;
+    int segundonumero;
+    char operador;
+    int resultado; 
+
+
+    printf("calculadora ->");
+    scanf("%99s", expressao);
+
+    printf("expressao recebida %s\n", expressao);
+
+
+
+
+    
+    return 0;    
+}
